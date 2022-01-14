4bc4086cd43f6f4d335a5295f3f497efd91d32c8
mariayasintameo <91182703+mariayasintameo@users.noreply.github.com>
2022-01-14 12:01:05 +0700

    Add files via upload

---
+++ Aplikasi.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi;
+
+/**
+ *
+ * @author ACER
+ */
+public class Aplikasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        register reg = new register();
+        reg.setVisible(true);             
+    }
+
+    private static class register {
+
+        public register() {
+        }
+
+        private void setVisible() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        private void setVisible(boolean b) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    
+}
